55753a70c9f509a0168ff9a161adb3b2b80b6959
birukabza <birukabza@gmail.com>
2025-07-27 22:24:01 +0300

    Use google cloud for translation and make the existing implementation  as a fallback

--- backend/src/services/transalation_service.py
+++ backend/src/services/transalation_service.py
@@ -1,10 +1,12 @@
+from google.cloud import translate_v2 as translate
 from deep_translator import GoogleTranslator, single_detection
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-class TranslationService:
+
+class TranslationServiceFallBack:
     def __init__(self):
         pass
 
@@ -18,4 +20,29 @@ class TranslationService:
         return GoogleTranslator(source="en", target=dest_lang).translate(text)
 
 
-translation_service = TranslationService()
+translation_fallback_service = TranslationServiceFallBack()
+
+
+class GoogleCloudTranslate:
+    def __init__(self):
+        self.translate_client = translate.Client.from_service_account_json(
+            "gcp_key.json"
+        )
+
+    def detect_language(self, text: str) -> str:
+        result = self.translate_client.detect_language(text)
+        # result['language'] is the detected language code
+        return result["language"]
+
+    def translate_to_english(self, text: str) -> str:
+        result = self.translate_client.translate(text, target_language="en")
+        return result["translatedText"]
+
+    def translate_from_english(self, text: str, dest_lang: str) -> str:
+        result = self.translate_client.translate(
+            text, source_language="en", target_language=dest_lang
+        )
+        return result["translatedText"]
+
+
+translation_service = GoogleCloudTranslate()
